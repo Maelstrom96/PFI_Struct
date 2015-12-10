27a329de7e73d5bf81d0d94db7c71391644a263a
maelstrom <alexandre.st-jacques@groupegodin.com>
2015-12-10 13:55:44 -0500

    Edit Case

--- ProjetDepartEtudiants/Case.cpp
+++ ProjetDepartEtudiants/Case.cpp
@@ -1,1 +1,17 @@
 #include "Case.h"
+
+Case::Case()
+{
+   NbCaseDispo = 0; // Initialisation à 0
+   NumPas = -1; // Pas est null
+}
+
+bool Case::EstLibre()
+{
+   return NumPas == -1;
+}
+
+int Case::Pas()
+{
+   return NumPas;
+}
--- ProjetDepartEtudiants/Case.h
+++ ProjetDepartEtudiants/Case.h
@@ -4,7 +4,11 @@ using namespace std;
 class Case
 {
    int NbCaseDispo;
+   int NumPas;
 
 public :
    Case();
+
+   bool EstLibre();
+   int Pas();
 };
