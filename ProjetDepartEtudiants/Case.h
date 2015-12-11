2d2cd83091935c247f6950017374fadab5fe5ff9
Maelstrom96 <alexandrovich8@live.ca>
2015-12-11 11:20:22 -0500

    Fix erreur de commit, fichier non modifier.

--- ProjetDepartEtudiants/Case.h
+++ ProjetDepartEtudiants/Case.h
@@ -3,8 +3,19 @@ using namespace std;
 
 class Case
 {
-   int NbCaseDispo;
+   int _x;
+   int _y;
+   int PriorityIndex;
 
-public :
-   Case();
+   void SetPrioriteIndex();
+
+public:
+   Case(int x, int y);
+   Case(const Case & original);
+   bool operator < (const Case& original) const;
+   ~Case();
+   int GetX() const { return _x; }
+   int GetY() const { return _y; }
+   void SetX(int x) { _x = x; }
+   void SetY(int y) { _y = y; };
 };
